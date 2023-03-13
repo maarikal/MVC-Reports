3df312657875c9fc586e411372112519714565c9
maarikal <maarika.lukk@khk.ee>
2023-03-13 13:48:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+## Report
+
+Report is a simple express app that allows you to see different financial and management reports, etc income statement,
+products overview.
+
+## Installation
+
+1. Clone the repo
+2. Copy .env.example to .env and fill in the values
+3. Run npm install
+4. Run npm start
+5. Go to localhost:3000
+
+## Documentation
+
+The documentation is available at http://localhost:3000/docs
+
+## Tests
+
+To run the tests, run npm test
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+// Swagger (or new word/usage for swagger is OpenAPI)
+const swaggerUI = require('swagger-ui-express');
+const yamlJs = require('yamljs')
+const swaggerDocument = yamlJs.load('./swagger.yaml');
+
+// Routes
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+// Parse body
+app.use(express.json());
+
+// Serve static files
+app.use(express.static('public'));
+
+// Error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    return res.status(err.statusCode || 500).send(err.message || 'Internal Server Error')
+})
+
+app.listen(port, () => console.log('Running at http://localhost:${port} and docs at https:// localhost:${port}/docs'));
